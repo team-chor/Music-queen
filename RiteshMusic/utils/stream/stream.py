7b4e48de0d5a87e643fadd2a5d1eea0632b3d26b
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-06-20 15:18:25 +0530

    ⟶̽ जय श्री ༢།म >𝟑🙏🚩

--- RiteshMusic/utils/stream/stream.py
+++ RiteshMusic/utils/stream/stream.py
@@ -1,3 +1,4 @@
+#⟶̽ जय श्री ༢།म >𝟑🙏🚩
 import os
 from random import randint
 from typing import Union
@@ -5,15 +6,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from DeadlineTech import Carbon, YouTube, app
-from DeadlineTech.core.call import Anony
-from DeadlineTech.misc import db
-from DeadlineTech.utils.database import add_active_video_chat, is_active_chat
-from DeadlineTech.utils.exceptions import AssistantErr
-from DeadlineTech.utils.inline import aq_markup, close_markup, stream_markup
-from DeadlineTech.utils.pastebin import AnonyBin
-from DeadlineTech.utils.stream.queue import put_queue, put_queue_index
-from DeadlineTech.utils.thumbnails import get_thumb
+from RiteshMusic import Carbon, YouTube, app
+from RiteshMusic.core.call import Anony
+from RiteshMusic.misc import db
+from RiteshMusic.utils.database import add_active_video_chat, is_active_chat
+from RiteshMusic.utils.exceptions import AssistantErr
+from RiteshMusic.utils.inline import aq_markup, close_markup, stream_markup
+from RiteshMusic.utils.pastebin import AnonyBin
+from RiteshMusic.utils.stream.queue import put_queue, put_queue_index
+from RiteshMusic.utils.thumbnails import get_thumb
 
 
 async def stream(
