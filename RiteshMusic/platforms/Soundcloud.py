f8eb028636cd350cc6854c80fecd3989bce434cd
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-06-20 19:03:55 +0530

    ⟶̽ जय श्री ༢།म >𝟑🙏🚩

--- RiteshMusic/platforms/Soundcloud.py
+++ RiteshMusic/platforms/Soundcloud.py
@@ -3,7 +3,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from DeadlineTech.utils.formatters import seconds_to_min
+from RiteshMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
