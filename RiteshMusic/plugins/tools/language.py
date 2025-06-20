7ba87115e41fa5745dfada594360483458c66bd8
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-06-20 18:47:36 +0530

    ⟶̽ जय श्री ༢།म >𝟑🙏🚩

--- RiteshMusic/plugins/tools/language.py
+++ RiteshMusic/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from DeadlineTech import app
-from DeadlineTech.utils.database import get_lang, set_lang
-from DeadlineTech.utils.decorators import ActualAdminCB, language, languageCB
+from RiteshMusic import app
+from RiteshMusic.utils.database import get_lang, set_lang
+from RiteshMusic.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
