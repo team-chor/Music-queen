917248c54424ed26e95438a1320fe2439aa27ca2
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-07-09 01:31:13 +0530

    जय श्री ༢།म >𝟑🙏🚩

--- RiteshMusic/utils/inline/help.py
+++ RiteshMusic/utils/inline/help.py
@@ -82,10 +82,6 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_14"],
                     callback_data="help_callback hb14",
                 ),
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
-                ),
             ],
             mark,
         ]
