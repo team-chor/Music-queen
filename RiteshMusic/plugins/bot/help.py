eef636a1fe7d00fceee574a85cf340edf5b18c8b
- ♔ 𝐒 𝐇 𝐈 𝐕 𝐀 𝐍 𝐆 🜲 ˹ 𝐎ᴘ ˼﹛🇨🇦﹜≈ 💸 <Shivmis165@gmail.com>
2025-07-09 01:51:44 +0530

    जय श्री ༢།म >𝟑🙏🚩

--- RiteshMusic/plugins/bot/help.py
+++ RiteshMusic/plugins/bot/help.py
@@ -88,5 +88,4 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(helpers.HELP_13, reply_markup=keyboard)
     elif cb == "hb14":
         await CallbackQuery.edit_message_text(helpers.HELP_14, reply_markup=keyboard)
-    elif cb == "hb15":
-        await CallbackQuery.edit_message_text(helpers.HELP_15, reply_markup=keyboard)
+    
